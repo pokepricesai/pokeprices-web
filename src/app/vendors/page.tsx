05dfab1db581c6c60fa3dc09d3db3577630135c9
pokepricesai <pokepricesai@gmail.com>
2026-03-11 23:30:04 +0000

    Create page.tsx

---
+++ src/app/vendors/page.tsx
@@ -0,0 +1,10 @@
+import VendorsPageClient from './VendorsPageClient'
+
+export const metadata = {
+  title: 'Card Shop Directory | PokePrices',
+  description: 'Find Pokémon card shops, online stores, eBay sellers and grading services near you.',
+}
+
+export default function VendorsPage() {
+  return <VendorsPageClient />
+}
