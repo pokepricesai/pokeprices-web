e92708a2d0d76666b1a3c9a265e6678a01ee1336
pokepricesai <pokepricesai@gmail.com>
2026-03-11 23:30:35 +0000

    Create page.tsx

---
+++ src/app/vendors/submit/page.tsx
@@ -0,0 +1,10 @@
+import VendorSubmitClient from './VendorSubmitClient'
+
+export const metadata = {
+  title: 'List Your Store | PokePrices Vendor Directory',
+  description: 'Add your Pokémon card shop, online store or eBay store to the PokePrices vendor directory for free.',
+}
+
+export default function VendorSubmitPage() {
+  return <VendorSubmitClient />
+}
